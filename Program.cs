9d3528e51172a32ef165e9fa278597c29237c82a
MayaMhemar <mayamkhemar13@gmail.com>
2022-12-12 22:27:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat("Marik", Gender.Male);
+
+
+            //test
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.CheckEnergy();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.CheckEnergy();
+            c1.Sleep();
+            c1.Sleep();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
